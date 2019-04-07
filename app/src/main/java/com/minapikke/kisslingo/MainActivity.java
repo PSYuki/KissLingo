55262f95b3b152c77f8a5219d9701be2a895051e
koheinishizawa <sasaran133-kisslingo@yahoo.co.jp>
2019-04-08 00:07:48 +0900

    add import

--- app/src/main/java/com/minapikke/kisslingo/MainActivity.java
+++ app/src/main/java/com/minapikke/kisslingo/MainActivity.java
@@ -2,6 +2,7 @@ package com.minapikke.kisslingo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
