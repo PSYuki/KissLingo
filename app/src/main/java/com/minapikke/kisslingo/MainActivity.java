aab861ea7a95988166542cd038c4da94a23f868b
koheinishizawa <sasaran133-kisslingo@yahoo.co.jp>
2019-04-29 00:01:10 +0900

    four spinner record search test

--- app/src/main/java/com/minapikke/kisslingo/MainActivity.java
+++ app/src/main/java/com/minapikke/kisslingo/MainActivity.java
@@ -30,6 +30,8 @@ public class MainActivity extends AppCompatActivity {
     private SQLiteDatabase DatabaseObject;
     //private SQLiteDatabase preDatabaseObject;
 
+    private String classStr;
+    private String wordStr;
     private String subjectStr;
     private String tenseStr;
 
@@ -46,29 +48,51 @@ public class MainActivity extends AppCompatActivity {
 
 
         // ▼spinner　onItemSelected設定▼
-        final Spinner spinner = (Spinner) findViewById(R.id.spinner);
+        final Spinner spinner4 = (Spinner) findViewById(R.id.spinner4);
+        final Spinner spinner5 = (Spinner) findViewById(R.id.spinner5);
+        final Spinner spinner6 = (Spinner) findViewById(R.id.spinner6);
+        final Spinner spinner7 = (Spinner) findViewById(R.id.spinner7);
 
-        ArrayAdapter<String> adapter
-                = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, getResources().getStringArray(R.array.tenseList));
+        ArrayAdapter<String> adapter4
+                = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, getResources().getStringArray(R.array.classList));
+        ArrayAdapter<String> adapter5
+                = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, getResources().getStringArray(R.array.wordVerbList));
+        ArrayAdapter<String> adapter6
+                = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, getResources().getStringArray(R.array.subjectList));
+        ArrayAdapter<String> adapter7
+                = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, getResources().getStringArray(R.array.tenseVerbList));
 
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        adapter4.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        adapter5.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        adapter6.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        adapter7.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
-        spinner.setAdapter(adapter);
+        spinner4.setAdapter(adapter4);
+        spinner5.setAdapter(adapter5);
+        spinner6.setAdapter(adapter6);
+        spinner7.setAdapter(adapter7);
 
         // spinner未選択時
-        tenseStr = getResources().getStringArray(R.array.tenseList)[0];
-        // default表示切替
-        spinner.setSelection(0, false);
+        classStr = getResources().getStringArray(R.array.classList)[0];
+        wordStr = getResources().getStringArray(R.array.wordVerbList)[0];
+        subjectStr = getResources().getStringArray(R.array.subjectList)[0];
+        tenseStr = getResources().getStringArray(R.array.tenseVerbList)[0];
 
-        // リスナーを登録
-        spinner.setOnItemSelectedListener(new OnItemSelectedListener() {
+        // default表示切替
+        spinner4.setSelection(0, false);
+        spinner5.setSelection(0, false);
+        spinner6.setSelection(0, false);
+        spinner7.setSelection(0, false);
+
+        // spinner4へリスナーを登録
+        spinner4.setOnItemSelectedListener(new OnItemSelectedListener() {
             //　アイテムが選択された時
             @Override
             public void onItemSelected(AdapterView<?> parent,
                                        View view, int position, long id) {
 
-                Spinner spinner = (Spinner)parent;
-                tenseStr = (String)spinner.getSelectedItem();
+                Spinner spinner4 = (Spinner)parent;
+                classStr = (String)spinner4.getSelectedItem();
             }
 
             //　アイテムが選択されなかった
@@ -76,6 +100,108 @@ public class MainActivity extends AppCompatActivity {
                 //
             }
         });
+
+        // spinner6へリスナーを登録
+        spinner6.setOnItemSelectedListener(new OnItemSelectedListener() {
+            //　アイテムが選択された時
+            @Override
+            public void onItemSelected(AdapterView<?> parent,
+                                       View view, int position, long id) {
+
+                Spinner spinner6 = (Spinner)parent;
+                tenseStr = (String)spinner6.getSelectedItem();
+            }
+
+            //　アイテムが選択されなかった
+            public void onNothingSelected(AdapterView<?> parent) {
+                //
+            }
+        });
+
+        //spinner5 and spinner7のclassStrによる分岐定義
+        if(classStr == "verb"){
+            adapter5 = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, getResources().getStringArray(R.array.wordVerbList));
+            adapter7 = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, getResources().getStringArray(R.array.tenseVerbList));
+
+            adapter5.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            adapter7.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+            spinner5.setAdapter(adapter5);
+            spinner7.setAdapter(adapter7);
+
+            wordStr = getResources().getStringArray(R.array.wordVerbList)[0];
+            tenseStr = getResources().getStringArray(R.array.tenseVerbList)[0];
+
+            spinner5.setSelection(0, false);
+            spinner7.setSelection(0, false);
+
+        }else if( classStr == "adjective") {
+            adapter5 = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, getResources().getStringArray(R.array.wordAdjectiveList));
+            adapter7 = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, getResources().getStringArray(R.array.tenseAdjectiveList));
+
+            adapter5.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            adapter7.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+            spinner5.setAdapter(adapter5);
+            spinner7.setAdapter(adapter7);
+
+            wordStr = getResources().getStringArray(R.array.wordVerbList)[0];
+            tenseStr = getResources().getStringArray(R.array.tenseVerbList)[0];
+
+            spinner5.setSelection(0, false);
+            spinner7.setSelection(0, false);
+        }else{
+            adapter5 = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, getResources().getStringArray(R.array.wordNounList));
+            adapter7 = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, getResources().getStringArray(R.array.tenseNounList));
+
+            adapter5.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            adapter7.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+            spinner5.setAdapter(adapter5);
+            spinner7.setAdapter(adapter7);
+
+            wordStr = getResources().getStringArray(R.array.wordNounList)[0];
+            tenseStr = getResources().getStringArray(R.array.tenseNounList)[0];
+
+            spinner5.setSelection(0, false);
+            spinner7.setSelection(0, false);
+        }
+
+        // リスナーを登録 spinner5
+        spinner5.setOnItemSelectedListener(new OnItemSelectedListener() {
+            //　アイテムが選択された時
+            @Override
+            public void onItemSelected(AdapterView<?> parent,
+                                       View view, int position, long id) {
+
+                Spinner spinner5 = (Spinner)parent;
+                wordStr = (String)spinner5.getSelectedItem();
+            }
+
+            //　アイテムが選択されなかった
+            public void onNothingSelected(AdapterView<?> parent) {
+                //
+            }
+        });
+
+        // リスナーを登録 spinner7
+        spinner7.setOnItemSelectedListener(new OnItemSelectedListener() {
+            //　アイテムが選択された時
+            @Override
+            public void onItemSelected(AdapterView<?> parent,
+                                       View view, int position, long id) {
+
+                Spinner spinner7 = (Spinner)parent;
+                tenseStr = (String)spinner7.getSelectedItem();
+            }
+
+            //　アイテムが選択されなかった
+            public void onNothingSelected(AdapterView<?> parent) {
+                //
+            }
+        });
+
+
         // ▼spinner　onItemSelected終了▼
 
 
@@ -90,7 +216,7 @@ public class MainActivity extends AppCompatActivity {
                             @Override
                             public void onClick(View v) {
                                 try {
-                                    String selectsql ="SELECT id,word,subject,tense,eng_example,jap_example FROM ExampleSentences WHERE tense ='" + tenseStr + "'";
+                                    String selectsql ="SELECT id,class,word,subject,tense,eng_example,jap_example FROM ExampleSentences WHERE class ='" + classStr + "' and word ='" + wordStr + "' and subject ='" + subjectStr + "' and tense ='" + tenseStr + "'";
 
                                     Cursor cursor = DatabaseObject.rawQuery(selectsql,null);
 
@@ -99,6 +225,7 @@ public class MainActivity extends AppCompatActivity {
                                     if(cursor.moveToFirst()){
                                         do{
                                             int id = cursor.getInt(cursor.getColumnIndex("id"));
+                                            //String class = cursor.getString(cursor.getColumnIndex("class"));
                                             String word = cursor.getString(cursor.getColumnIndex("word"));
                                             String subject = cursor.getString(cursor.getColumnIndex("subject"));
                                             String tense = cursor.getString(cursor.getColumnIndex("tense"));
@@ -136,21 +263,21 @@ public class MainActivity extends AppCompatActivity {
             String dropTable = "DROP TABLE IF EXISTS " + DB_TABLE;
 
             String createTable = "CREATE TABLE " + DB_TABLE +
-                    "(id integer primary key autoincrement, word text, subject text, tense text, eng_example text, jap_example text)";
+                    "(id integer primary key autoincrement, class text, word text, subject text, tense text, eng_example text, jap_example text)";
 
             String[] insertData = {
-                    "INSERT INTO " + DB_TABLE + "(word, subject, tense, eng_example, jap_example) VALUES ('study', 'I', '現在・肯定', 'I study English every day.', '私は毎日英語を勉強する')",
-                    "INSERT INTO " + DB_TABLE + "(word, subject, tense, eng_example, jap_example) VALUES ('study', 'I', '現在・肯定', 'Do I study English every day?', '私は毎日英語を勉強する？')",
-                    "INSERT INTO " + DB_TABLE + "(word, subject, tense, eng_example, jap_example) VALUES ('study', 'I', '現在・肯定', 'What do I study every day?', '私は毎日何を勉強する？')",
-                    "INSERT INTO " + DB_TABLE + "(word, subject, tense, eng_example, jap_example) VALUES ('study', 'I', '現在・否定', 'I don’t study it at school.', '私はそれを学校で勉強しない')",
-                    "INSERT INTO " + DB_TABLE + "(word, subject, tense, eng_example, jap_example) VALUES ('study', 'I', '現在・否定', 'Don’t I study it at school?', '私はそれを学校で勉強しない？')",
-                    "INSERT INTO " + DB_TABLE + "(word, subject, tense, eng_example, jap_example) VALUES ('study', 'I', '現在・否定', 'Why don’t I study it at school?', '私はなぜそれを学校で勉強しない？')",
-                    "INSERT INTO " + DB_TABLE + "(word, subject, tense, eng_example, jap_example) VALUES ('study', 'I', '過去・肯定', 'I studied math last night.', '私は昨夜数学を勉強した')",
-                    "INSERT INTO " + DB_TABLE + "(word, subject, tense, eng_example, jap_example) VALUES ('study', 'I', '過去・肯定', 'Did I study math last night?', '私は昨夜数学を勉強した？')",
-                    "INSERT INTO " + DB_TABLE + "(word, subject, tense, eng_example, jap_example) VALUES ('study', 'I', '過去・肯定', 'What did I study last night?', '私は昨夜何を勉強した？')",
-                    "INSERT INTO " + DB_TABLE + "(word, subject, tense, eng_example, jap_example) VALUES ('study', 'I', '過去・否定', 'I didn’t study it this morning.', '私は今朝それを勉強しなかった')",
-                    "INSERT INTO " + DB_TABLE + "(word, subject, tense, eng_example, jap_example) VALUES ('study', 'I', '過去・否定', 'Didn’t I study it this morning?', '私は今朝それを勉強しなかった？')",
-                    "INSERT INTO " + DB_TABLE + "(word, subject, tense, eng_example, jap_example) VALUES ('study', 'I', '過去・否定', 'Why didn’t I study it this morning?', '私はなぜ今朝それを勉強しなかった？')"
+                    "INSERT INTO " + DB_TABLE + "(class, word, subject, tense, eng_example, jap_example) VALUES ('verb','study', 'I', '現在・肯定', 'I study English every day.', '私は毎日英語を勉強する')",
+                    "INSERT INTO " + DB_TABLE + "(class, word, subject, tense, eng_example, jap_example) VALUES ('verb','study', 'I', '現在・肯定', 'Do I study English every day?', '私は毎日英語を勉強する？')",
+                    "INSERT INTO " + DB_TABLE + "(class, word, subject, tense, eng_example, jap_example) VALUES ('verb','study', 'I', '現在・肯定', 'What do I study every day?', '私は毎日何を勉強する？')",
+                    "INSERT INTO " + DB_TABLE + "(class, word, subject, tense, eng_example, jap_example) VALUES ('verb','study', 'I', '現在・否定', 'I don’t study it at school.', '私はそれを学校で勉強しない')",
+                    "INSERT INTO " + DB_TABLE + "(class, word, subject, tense, eng_example, jap_example) VALUES ('verb','study', 'I', '現在・否定', 'Don’t I study it at school?', '私はそれを学校で勉強しない？')",
+                    "INSERT INTO " + DB_TABLE + "(class, word, subject, tense, eng_example, jap_example) VALUES ('verb','study', 'I', '現在・否定', 'Why don’t I study it at school?', '私はなぜそれを学校で勉強しない？')",
+                    "INSERT INTO " + DB_TABLE + "(class, word, subject, tense, eng_example, jap_example) VALUES ('verb','study', 'I', '過去・肯定', 'I studied math last night.', '私は昨夜数学を勉強した')",
+                    "INSERT INTO " + DB_TABLE + "(class, word, subject, tense, eng_example, jap_example) VALUES ('verb','study', 'I', '過去・肯定', 'Did I study math last night?', '私は昨夜数学を勉強した？')",
+                    "INSERT INTO " + DB_TABLE + "(class, word, subject, tense, eng_example, jap_example) VALUES ('verb','study', 'I', '過去・肯定', 'What did I study last night?', '私は昨夜何を勉強した？')",
+                    "INSERT INTO " + DB_TABLE + "(class, word, subject, tense, eng_example, jap_example) VALUES ('verb','study', 'I', '過去・否定', 'I didn’t study it this morning.', '私は今朝それを勉強しなかった')",
+                    "INSERT INTO " + DB_TABLE + "(class, word, subject, tense, eng_example, jap_example) VALUES ('verb','study', 'I', '過去・否定', 'Didn’t I study it this morning?', '私は今朝それを勉強しなかった？')",
+                    "INSERT INTO " + DB_TABLE + "(class, word, subject, tense, eng_example, jap_example) VALUES ('verb','study', 'I', '過去・否定', 'Why didn’t I study it this morning?', '私はなぜ今朝それを勉強しなかった？')"
             };
 
             // 古いテーブルを破棄
@@ -186,7 +313,7 @@ public class MainActivity extends AppCompatActivity {
         db.execSQL(
                 "CREATE TABLE IF NOT EXISTS " +
                         DB_TABLE +
-                        "(id integer primary key autoincrement, word text, subject text, tense text, eng_example text, jap_example text)"
+                        "(id integer primary key autoincrement, class text, word text, subject text, tense text, eng_example text, jap_example text)"
         );
         Log.d("Database","Create Table");
         }
