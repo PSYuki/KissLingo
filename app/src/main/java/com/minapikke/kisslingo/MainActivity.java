20ce7557dfac2b75810200b963460706444386fb
koheinishizawa <sasaran133-kisslingo@yahoo.co.jp>
2019-06-05 01:25:36 +0900

    csv test

--- app/src/main/java/com/minapikke/kisslingo/MainActivity.java
+++ app/src/main/java/com/minapikke/kisslingo/MainActivity.java
@@ -39,10 +39,11 @@ public class MainActivity extends AppCompatActivity {
     private SQLiteDatabase DatabaseObject;
     //private SQLiteDatabase preDatabaseObject;
 
+    private String ylangStr;
+    private String tlangStr;
+    private String levelStr;
     private String wclassStr;
-    private String wordStr;
-    private String subjectStr;
-    private String tenseStr;
+    private String typeStr;
 
 
     @Override
@@ -63,74 +64,138 @@ public class MainActivity extends AppCompatActivity {
         String dropTable = "DROP TABLE IF EXISTS " + DB_TABLE;
 
         String createTable = "CREATE TABLE " + DB_TABLE +
-                "(id integer primary key autoincrement, ylang text, tlang text, level text, wclass text, word text, subject text, tense text, type text, ylang_ex text, tlang_ex text, tlang_exf, furigana text, chikugoyaku text)";
+                "(id integer primary key autoincrement, ylang text, tlang text, level text, wclass text, type text, tlang_ex text, ylang_ex text, tlang_exf, furigana text, chikugoyaku text)";
 
         // 古いテーブルを破棄
         DatabaseObject.execSQL(dropTable);
         // テーブルを作成
         DatabaseObject.execSQL(createTable);
 
-       /* try {
-            //for(int i=1; i<4; i++) {
-
-                AssetManager assetManager = getApplicationContext().getAssets();
-                //InputStream inputStream = assetManager.open("jap_verb" + i + ".csv");
-                InputStream inputStream = assetManager.open("jap_verb.csv");
-                InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
-                BufferedReader bufferReader = new BufferedReader(inputStreamReader);
-                String line;
-                while ((line = bufferReader.readLine()) != null) {
-                    String[] RowData = line.split(",");
-                    String InsertRowData =
-                            "INSERT INTO " + DB_TABLE + "(ylang, tlang, level, wclass, word, subject, tense, type, ylang_ex, tlang_ex, tlang_exf, furigana, chikugoyaku) VALUES ('" + RowData[1] + "','" + RowData[2] + "','" + RowData[3] + "','" + RowData[4] + "','" + RowData[5] + "','" + RowData[6] + "','" + RowData[7] + "','" + RowData[8] + "','" + RowData[9] + "','" + RowData[10] + "','" + RowData[11] + "','" + RowData[12] + "','" + RowData[13] + "')";
-
-                    DatabaseObject.execSQL(InsertRowData);
-                }
-            //}
-        } catch (IOException e) {
-            e.printStackTrace();
-        } */
-
+       // csvからのデータの書き込み
         writetoDatabase("jap_verb.csv");
-        writetoDatabase("jap_adjective.csv");
         writetoDatabase("eng_verb.csv");
 
+
         // ▼spinner　onItemSelected設定▼
+        final Spinner spinner1 = (Spinner) findViewById(R.id.spinner1);
+        final Spinner spinner2 = (Spinner) findViewById(R.id.spinner2);
+        final Spinner spinner3 = (Spinner) findViewById(R.id.spinner3);
         final Spinner spinner4 = (Spinner) findViewById(R.id.spinner4);
         final Spinner spinner5 = (Spinner) findViewById(R.id.spinner5);
-        final Spinner spinner6 = (Spinner) findViewById(R.id.spinner6);
-        final Spinner spinner7 = (Spinner) findViewById(R.id.spinner7);
 
+        ArrayAdapter<String> adapter1
+                = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, getResources().getStringArray(R.array.YL));
+        ArrayAdapter<String> adapter2
+                = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, getResources().getStringArray(R.array.TLE));
+        ArrayAdapter<String> adapter3
+                = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, getResources().getStringArray(R.array.LJ));
         ArrayAdapter<String> adapter4
-                = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, getResources().getStringArray(R.array.CLJ));
+                = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, getResources().getStringArray(R.array.CJ));
         ArrayAdapter<String> adapter5
-                = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, getResources().getStringArray(R.array.WVLE));
-        ArrayAdapter<String> adapter6
-                = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, getResources().getStringArray(R.array.SLE));
-        ArrayAdapter<String> adapter7
-                = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, getResources().getStringArray(R.array.TVLE));
+                = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, getResources().getStringArray(R.array.TJ));
 
+        adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        adapter3.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         adapter4.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         adapter5.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        adapter6.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        adapter7.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
+        spinner1.setAdapter(adapter1);
+        spinner2.setAdapter(adapter2);
+        spinner3.setAdapter(adapter3);
         spinner4.setAdapter(adapter4);
         spinner5.setAdapter(adapter5);
-        spinner6.setAdapter(adapter6);
-        spinner7.setAdapter(adapter7);
 
         // spinner未選択時
-        wclassStr = getResources().getStringArray(R.array.CLJ)[0];
-        wordStr = getResources().getStringArray(R.array.WVLE)[0];
-        subjectStr = getResources().getStringArray(R.array.SLE)[0];
-        tenseStr = getResources().getStringArray(R.array.TVLE)[0];
+        ylangStr = getResources().getStringArray(R.array.YL)[0];
+        tlangStr = getResources().getStringArray(R.array.TLE)[0];
+        levelStr = getResources().getStringArray(R.array.LJ)[0];
+        wclassStr = getResources().getStringArray(R.array.CJ)[0];
+        typeStr = getResources().getStringArray(R.array.TJ)[0];
 
         // default表示切替
+        spinner1.setSelection(0, false);
+        spinner2.setSelection(0, false);
+        spinner3.setSelection(0, false);
         spinner4.setSelection(0, false);
         spinner5.setSelection(0, false);
-        spinner6.setSelection(0, false);
-        spinner7.setSelection(0, false);
+
+        // spinner1へリスナーを登録
+        spinner1.setOnItemSelectedListener(new OnItemSelectedListener() {
+            //　アイテムが選択された時
+            @Override
+            public void onItemSelected(AdapterView<?> parent,
+                                       View view, int position, long id) {
+
+                Spinner spinner1 = (Spinner)parent;
+                ylangStr = (String)spinner1.getSelectedItem();
+
+                //spinner2-5 のylangStrによる分岐定義
+                if(ylangStr.equals("English")){
+                    setSpinner(spinner2, getResources().getStringArray(R.array.TLE));
+                    setSpinner(spinner3, getResources().getStringArray(R.array.LJ));
+                    setSpinner(spinner4, getResources().getStringArray(R.array.CJ));
+                    setSpinner(spinner5, getResources().getStringArray(R.array.TJ));
+                }else{
+                    setSpinner(spinner2, getResources().getStringArray(R.array.TLJ));
+                    setSpinner(spinner3, getResources().getStringArray(R.array.LE));
+                    setSpinner(spinner4, getResources().getStringArray(R.array.CE));
+                    setSpinner(spinner5, getResources().getStringArray(R.array.TE));
+                }
+                /*else if( ylang.equals("日本語")) {
+                    setSpinner(spinner2, getResources().getStringArray(R.array.TLJ));
+                    setSpinner(spinner3, getResources().getStringArray(R.array.LE));
+                    setSpinner(spinner4, getResources().getStringArray(R.array.CE));
+                    setSpinner(spinner5, getResources().getStringArray(R.array.TE));
+                }else{
+                    //ここで、spinner5を画面から消去し、spinner6をsubjectNounList（this/this noun / I / mine etc）に変更したい
+                    setSpinner(spinner5, getResources().getStringArray(R.array.WNLE));
+                    setSpinner(spinner7, getResources().getStringArray(R.array.TBLJ));
+                }*/
+
+            }
+
+            //　アイテムが選択されなかった
+            public void onNothingSelected(AdapterView<?> parent) {
+                //
+            }
+
+        });
+
+        // spinner2へリスナーを登録
+        spinner2.setOnItemSelectedListener(new OnItemSelectedListener() {
+            //　アイテムが選択された時
+            @Override
+            public void onItemSelected(AdapterView<?> parent,
+                                       View view, int position, long id) {
+
+                Spinner spinner2 = (Spinner)parent;
+                tlangStr = (String)spinner2.getSelectedItem();
+            }
+
+            //　アイテムが選択されなかった
+            public void onNothingSelected(AdapterView<?> parent) {
+                //
+            }
+        });
+
+
+        // spinner3へリスナーを登録
+        spinner3.setOnItemSelectedListener(new OnItemSelectedListener() {
+            //　アイテムが選択された時
+            @Override
+            public void onItemSelected(AdapterView<?> parent,
+                                       View view, int position, long id) {
+
+                Spinner spinner3 = (Spinner)parent;
+                levelStr = (String)spinner3.getSelectedItem();
+            }
+
+            //　アイテムが選択されなかった
+            public void onNothingSelected(AdapterView<?> parent) {
+                //
+            }
+        });
 
         // spinner4へリスナーを登録
         spinner4.setOnItemSelectedListener(new OnItemSelectedListener() {
@@ -141,44 +206,6 @@ public class MainActivity extends AppCompatActivity {
 
                 Spinner spinner4 = (Spinner)parent;
                 wclassStr = (String)spinner4.getSelectedItem();
-
-                //spinner5 and spinner7のclassStrによる分岐定義
-                if(wclassStr.equals("動詞")){
-                    setSpinner(spinner5, getResources().getStringArray(R.array.WVLE));
-                    setSpinner(spinner7, getResources().getStringArray(R.array.TVLE));
-                }else if( wclassStr.equals("形容詞")) {
-                    setSpinner(spinner5, getResources().getStringArray(R.array.WALE));
-                    setSpinner(spinner7, getResources().getStringArray(R.array.TALJ));
-                }else{
-                    //ここで、spinner5を画面から消去し、spinner6をsubjectNounList（this/this noun / I / mine etc）に変更したい
-                    setSpinner(spinner5, getResources().getStringArray(R.array.WNLE));
-                    setSpinner(spinner7, getResources().getStringArray(R.array.TNLJ));
-                }
-
-            }
-
-            //　アイテムが選択されなかった
-            public void onNothingSelected(AdapterView<?> parent) {
-                //
-            }
-
-            private void setSpinner(Spinner spinner,String[] arr){
-                ArrayAdapter<String> adapter = new ArrayAdapter<>(MainActivity.this, android.R.layout.simple_spinner_item, arr);
-                adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-                spinner.setAdapter(adapter);
-                spinner.setSelection(0, false);
-            }
-        });
-
-        // spinner6へリスナーを登録
-        spinner6.setOnItemSelectedListener(new OnItemSelectedListener() {
-            //　アイテムが選択された時
-            @Override
-            public void onItemSelected(AdapterView<?> parent,
-                                       View view, int position, long id) {
-
-                Spinner spinner6 = (Spinner)parent;
-                subjectStr = (String)spinner6.getSelectedItem();
             }
 
             //　アイテムが選択されなかった
@@ -187,8 +214,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-
-        // リスナーを登録 spinner5
+        // spinner5へリスナーを登録
         spinner5.setOnItemSelectedListener(new OnItemSelectedListener() {
             //　アイテムが選択された時
             @Override
@@ -196,24 +222,7 @@ public class MainActivity extends AppCompatActivity {
                                        View view, int position, long id) {
 
                 Spinner spinner5 = (Spinner)parent;
-                wordStr = (String)spinner5.getSelectedItem();
-            }
-
-            //　アイテムが選択されなかった
-            public void onNothingSelected(AdapterView<?> parent) {
-                //
-            }
-        });
-
-        // リスナーを登録 spinner7
-        spinner7.setOnItemSelectedListener(new OnItemSelectedListener() {
-            //　アイテムが選択された時
-            @Override
-            public void onItemSelected(AdapterView<?> parent,
-                                       View view, int position, long id) {
-
-                Spinner spinner7 = (Spinner)parent;
-                tenseStr = (String)spinner7.getSelectedItem();
+                wclassStr = (String)spinner5.getSelectedItem();
             }
 
             //　アイテムが選択されなかった
@@ -237,7 +246,7 @@ public class MainActivity extends AppCompatActivity {
                             @Override
                             public void onClick(View v) {
                                 try {
-                                    String selectsql ="SELECT id,ylang,tlang,level,wclass,word,subject,tense,type,ylang_ex,tlang_ex,tlang_exf,furigana,chikugoyaku FROM ExampleSentences";
+                                    String selectsql ="SELECT id,ylang,tlang,level,wclass,type,tlang_ex,ylang_ex,tlang_exf,furigana,chikugoyaku FROM ExampleSentences";
                                     //String selectsql ="SELECT id,ylang,tlang,level,wclass,word,subject,tense,type,ylang_ex,tlang_ex,tlang_exf,furigana,chikugoyaku FROM ExampleSentences WHERE class ='" + wclassStr + "' and word ='" + wordStr + "' and subject ='" + subjectStr + "' and tense ='" + tenseStr + "'";
 
                                     Cursor cursor = DatabaseObject.rawQuery(selectsql,null);
@@ -248,13 +257,14 @@ public class MainActivity extends AppCompatActivity {
                                         do{
                                             int id = cursor.getInt(cursor.getColumnIndex("id"));
                                             String wclass = cursor.getString(cursor.getColumnIndex("wclass"));
-                                            String word = cursor.getString(cursor.getColumnIndex("word"));
-                                            String subject = cursor.getString(cursor.getColumnIndex("subject"));
-                                            String tense = cursor.getString(cursor.getColumnIndex("tense"));
-                                            String ylang_ex = cursor.getString(cursor.getColumnIndex("ylang_ex"));
+                                            //String word = cursor.getString(cursor.getColumnIndex("word"));
+                                            //String subject = cursor.getString(cursor.getColumnIndex("subject"));
+                                            //String tense = cursor.getString(cursor.getColumnIndex("tense"));
+                                            String type = cursor.getString(cursor.getColumnIndex("type"));
                                             String tlang_ex = cursor.getString(cursor.getColumnIndex("tlang_ex"));
+                                            String ylang_ex = cursor.getString(cursor.getColumnIndex("ylang_ex"));
 
-                                            String row = id + ":" + word + ":" + subject + ":" + tense + ":" + ylang_ex + ":" + tlang_ex;
+                                            String row = id + ":" + type + ":" + ylang_ex + ":" + tlang_ex;
                                             //String row = tense + ":" ;
                                             ad.add(row);
                                         }while(cursor.moveToNext());
@@ -271,19 +281,27 @@ public class MainActivity extends AppCompatActivity {
 
                             //　▼onCreate method終了▼
 
+    // setSpinnerの定義
+    private void setSpinner(Spinner spinner,String[] arr){
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(MainActivity.this, android.R.layout.simple_spinner_item, arr);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setSelection(0, false);
+    }
 
+    // writetoDatabaseの定義
     private void writetoDatabase(String filename) {
         try{
             AssetManager assetManager = getApplicationContext().getAssets();
             //InputStream inputStream = assetManager.open("jap_verb" + i + ".csv");
-            InputStream inputStream = assetManager.open("filename.csv");
+            InputStream inputStream = assetManager.open(filename);
             InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
             BufferedReader bufferReader = new BufferedReader(inputStreamReader);
             String line;
             while ((line = bufferReader.readLine()) != null) {
                 String[] arrr = line.split(",");
                 String Insertarrr =
-                        "INSERT INTO " + DB_TABLE + "(ylang, tlang, level, wclass, word, subject, tense, type, ylang_ex, tlang_ex, tlang_exf, furigana, chikugoyaku) VALUES ('" + arrr[1] + "','" + arrr[2] + "','" + arrr[3] + "','" + arrr[4] + "','" + arrr[5] + "','" + arrr[6] + "','" + arrr[7] + "','" + arrr[8] + "','" + arrr[9] + "','" + arrr[10] + "','" + arrr[11] + "','" + arrr[12] + "','" + arrr[13] + "')";
+                        "INSERT INTO " + DB_TABLE + "(ylang, tlang, level, wclass, type, tlang_ex, ylang_ex, tlang_exf, furigana, chikugoyaku) VALUES ('" + arrr[1] + "','" + arrr[2] + "','" + arrr[3] + "','" + arrr[4] + "','" + arrr[5] + "','" + arrr[6] + "','" + arrr[7] + "','" + arrr[8] + "','" + arrr[9] + "','" + arrr[10] + "')";
 
                 DatabaseObject.execSQL(Insertarrr);
 
@@ -305,7 +323,7 @@ public class MainActivity extends AppCompatActivity {
             String dropTable = "DROP TABLE IF EXISTS " + DB_TABLE;
 
             String createTable = "CREATE TABLE " + DB_TABLE +
-                    "(id integer primary key autoincrement, ylang text, tlang text, level text, wclass text, word text, subject text, tense text, type text, ylang_ex text, tlang_ex text, tlang_exf, furigana text, chikugoyaku text)";
+                    "(id integer primary key autoincrement, ylang text, tlang text, level text, wclass text, type text, tlang_ex text, ylang_ex text, tlang_exf, furigana text, chikugoyaku text)";
 
             String[] insertData = {
                     "INSERT INTO " + DB_TABLE + "(ylang, tlang, level, wclass, word, subject, tense, type, eng_example, jap_example) VALUES ('日本語','英語', '初級','動詞','study', 'I', '現在・肯定', 'I study English every day.', '私は毎日英語を勉強する')",
@@ -355,7 +373,7 @@ public class MainActivity extends AppCompatActivity {
         db.execSQL(
                 "CREATE TABLE IF NOT EXISTS " +
                         DB_TABLE +
-                        "(id integer primary key autoincrement, ylang text, tlang text, level text, wclass text, word text, subject text, tense text, type text, ylang_ex text, tlang_ex text, tlang_exf, furigana text, chikugoyaku text)"
+                        "(id integer primary key autoincrement, ylang text, tlang text, level text, wclass text, type text, tlang_ex text, ylang_ex text, tlang_exf, furigana text, chikugoyaku text)"
         );
         Log.d("Database","Create Table");
         }
